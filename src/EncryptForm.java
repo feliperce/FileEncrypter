8966120570d6720ed8758b40af6f6308c2a222fd
Felipe Rodrigues <felipe.celestino@ftd.com.br>
2018-10-31 13:49:39 -0300

    changed form

--- src/EncryptForm.java
+++ src/EncryptForm.java
@@ -2,11 +2,22 @@ import javax.swing.*;
 
 public class EncryptForm {
     private JTextField fileTextField;
-    private JButton cryptButton;
-    private JPanel panel1;
+    private JButton encryptButton;
+    private JPanel mainPanel;
     private JTextField cryptTextField;
+    private JButton decryptButton;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("EncryptForm");
+        frame.setContentPane(new EncryptForm().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
     }
 }
